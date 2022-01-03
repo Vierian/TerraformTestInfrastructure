74f71c9e1623556543752ac16d5242ec240c4157
AILLERON\luje <luje@softwaremind.com>
2022-01-03 17:24:13 +0100

    Change for mysql connection

--- modules/RDS/main.tf
+++ modules/RDS/main.tf
@@ -2,8 +2,8 @@ resource "aws_db_instance" "education" {
   identifier             = "education"
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
-  engine                 = "postgres"
-  engine_version         = "13.1"
+  engine                 = "mysql"
+  engine_version         = "8.0.20"
   username               = "edu"
   password               = var.db_password
   db_subnet_group_name   = var.sn_rds_id
@@ -11,11 +11,12 @@ resource "aws_db_instance" "education" {
   parameter_group_name   = var.sn_rds_id
   publicly_accessible    = false
   skip_final_snapshot    = true
+  port                   = 5432
 }
 
 resource "aws_db_parameter_group" "education" {
   name   = "education"
-  family = "postgres13"
+  family = "mysql8.0"
 
   parameter {
     name  = "log_connections"
